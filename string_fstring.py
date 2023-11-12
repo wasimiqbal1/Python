cb268a484950a9fd69e90f8f05ab7b5afa4692ac
wasimiqbal1 <wasim_iqbal@msn.com>
2023-11-12 14:38:23 +0500

    f-String

---
+++ string_fstring.py
@@ -0,0 +1,13 @@
+name : str = "Wasim Iqbal"
+fname : str = "Muhammad Iqbal"
+education : str = "Graduation"
+age : int = "42"
+
+card : str = f"""
+PIAIC Student Card
+Student Name: {name}
+Father Name : {fname}
+Age: {age}
+Education: {education}
+"""
+print(card)
